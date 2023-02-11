f476276a15eff70a80de50715c9efbb982be5f2e
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-02-11 21:10:25 +0000

    start the project and create header

---
+++ app/Header.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import React from 'react';
+import { HiBars3 } from 'react-icons/hi2';
+function Header() {
+  return (
+    <div className="grid grid-cols-3 p-10 items-center">
+      <HiBars3 className=" h-8 w-8 cursor-pointer" />
+      <Link href="/" prefetch={false}>
+        <h3 className="font-serif text-4xl text-center">
+          Daily{' '}
+          <span className="underline decoration-6 decoration-orange-400">
+            Nicki
+          </span>{' '}
+          news
+        </h3>
+      </Link>
+      <div className="flex items-center justify-end space-x-2">
+        {/* {darkmode butoon} */}
+
+        <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 rounded-full dark:bg-slate-800">
+          subscribe Now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-import './globals.css'
+import './globals.css';
+import Header from './Header';
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +9,11 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Header />
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
