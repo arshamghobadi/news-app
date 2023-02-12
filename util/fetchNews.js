fe4301ad338e4827d74ec69a046d4cdc9d9cbf40
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-02-12 22:30:06 +0000

    before fetch data

---
+++ util/fetchNews.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function fetchNews() {
+  return <div>fetchNews</div>;
+}
+
+export default fetchNews;
+
+// stepzen import curl http://api.mediastack.com/v1/news?7ee0f7cb089685d59e4aa7af247ffa7f
